8ba72547e359b9857e4023dfd56928da761080b7
Chanel Mepschen <chanelmepschen@013.155.028.145.hva.nl>
2017-03-02 10:40:49 +0100

    Added form validation, made picture with subtitle kind of full screen

--- w2_componenten/script.js
+++ w2_componenten/script.js
@@ -2,3 +2,15 @@
 setTimeout(function(){
     document.querySelector('.loadingscreen').hidden = true;
 }, 1000);
+
+document.querySelector('.figure').addEventListener('click', function(){
+    if(document.querySelector('.figure h1').className == 'opacity'){
+        document.querySelector('.figure h1').classList.remove('opacity');
+        document.querySelector('figcaption').classList.remove('opacity');
+        document.querySelector('body').classList.remove('colorchange');
+    } else {
+        document.querySelector('.figure h1').className += 'opacity';
+        document.querySelector('figcaption').className += 'opacity';
+        document.querySelector('body').className += 'colorchange';
+    }
+});
