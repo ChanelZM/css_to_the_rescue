cbbeda9f8256dcbd410993647e68f0bfa0cd9b65
Chanel Mepschen <chanelmepschen856@hotmail.com>
2017-05-21 14:33:59 +0200

    Created a website to add all the components, created new style

--- w3_website/js/script.js
+++ w3_website/js/script.js
@@ -1,20 +1,26 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 
-var elements = {
-    menu : document.querySelector('.menu'),
-    menuButton : document.querySelector('button')
-};
+(function(){
+    var elements = {
+        menu : document.querySelector('.menu'),
+        menuButton : document.querySelector('.togglemenu')
+    };
 
-elements.menu.hidden = true;
+    var app = {
+        init: function(){
+            elements.menu.classList.add('hide');
+            elements.menu.classList.add('menushow');
+        }
+    };
 
-elements.menuButton.addEventListener('click', function(){
-    if(elements.menu.hidden != true){
-        elements.menu.hidden = true;
-    } else {
-        elements.menu.hidden = false;
-    }
-});
+    elements.menuButton.addEventListener('click', function(){
+        if(elements.menu.getAttribute('class').includes('hide') == true){
+            elements.menu.classList.remove('hide');
+        } else {
+            elements.menu.classList.add('hide');
+        }
+    });
 
-elements.menu.addEventListener('click', function(){
-    elements.menu.hidden = true;
-});
+    app.init();
+
+})();
